c675d6b1ed755a1f3c4277645600a73a21dca20c
Josh Girvin <josh@jgirvin.com>
2015-12-01 21:53:31 +1000

    Added better error handling and renderer

--- res1.nim
+++ res1.nim
@@ -1,17 +1,24 @@
 import sdl2
 import res_path
 
-when isMainModule:
-  if int(sdl2.init(sdl2.INIT_VIDEO)) != 0:
-    echo("SDL2 Error: " & $sdl2.getError())
-    quit(1)
+proc quitWithError(ctx: string = "SDL") =
+  echo(ctx & " Error: " & $sdl2.getError())
+  quit(1)
 
-  var
-    win: WindowPtr = sdl2.createWindow("Hello World!", 100, 100, 640, 480, sdl2.SDL_WINDOW_SHOWN)
+proc run() =
+  # Initialise SDL2 video subsystem
+  if int(sdl2.init(INIT_VIDEO)) != 0:
+    quitWithError()
 
+  # Create our window and hang on to a pointer
+  var win = sdl2.createWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN)
   if win == nil:
-    echo("SDL2 Error: " & $sdl2.getError())
-    quit(1)
+    quitWithError("CreateWindow")
 
-  delay(2000)
-  quit(0)
+  # Create our renderer
+  var ren = sdl2.createRenderer(win, -1, sdl2.RENDERER_ACCELERATED and sdl2.RENDERER_PRESENTVSYNC)
+  if ren == nil:
+    quitWithError("CreateRenderer")
+
+# Run our game
+when isMainModule: run()
