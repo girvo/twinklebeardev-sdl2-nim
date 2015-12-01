5ebd7794d0429a008a1139bb49add1a4badfa6e7
Josh Girvin <josh@jgirvin.com>
2015-12-01 22:08:22 +1000

    Rendering with a pretty inefficient loop!

--- res1.nim
+++ res1.nim
@@ -14,24 +14,38 @@ proc run() =
     quitWithError()
 
   # Create our window and hang on to a pointer
-  var win = sdl2.createWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN)
+  var win: WindowPtr = sdl2.createWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN)
   if win == nil:
     quitWithError("CreateWindow")
 
   # Create our renderer
-  var ren = sdl2.createRenderer(win, -1, sdl2.RENDERER_ACCELERATED and sdl2.RENDERER_PRESENTVSYNC)
+  var ren: RendererPtr = sdl2.createRenderer(win, -1, sdl2.RENDERER_ACCELERATED and sdl2.RENDERER_PRESENTVSYNC)
   if ren == nil:
     quitWithError("CreateRenderer", true)
 
   # Load a surface
   var
     imagePath: string = getResourcePath("Lesson1") & DirSep & "hello.bmp"
-    bmp: SurfacePtr = sdl2.LoadBmp(imagePath)
-
+    bmp: SurfacePtr = sdl2.loadBmp(imagePath)
   if bmp == nil:
     sdl2.destroy(ren)
     sdl2.destroy(win)
     quitWithError("LoadBmp")
+
+  # Create our texture
+  var tex: TexturePtr = sdl2.createTextureFromSurface(ren, bmp)
+  sdl2.freeSurface(bmp)
+  if tex == nil:
+    sdl2.destroy(ren)
+    sdl2.destroy(win)
+    quitWithError("CreateTextureFromSurface")
+
+  # A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+  for i in 0..3:
+    ren.clear()
+    ren.copy(tex, nil, nil)
+    ren.present()
+    sdl2.delay(1000)
 
 # Run our game
 when isMainModule: run()
