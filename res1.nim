d393460888f43b7ef635fc3d28627404645b7e62
Josh Girvin <josh@jgirvin.com>
2015-12-01 21:42:47 +1000

    Have window opening now

--- res1.nim
+++ res1.nim
@@ -1,6 +1,17 @@
 import sdl2
 import res_path
 
-if int(sdl2.init(sdl2.INIT_VIDEO)) != 0:
-  echo("SDL2 Error: " & $sdl2.getError())
-  quit(1)
+when isMainModule:
+  if int(sdl2.init(sdl2.INIT_VIDEO)) != 0:
+    echo("SDL2 Error: " & $sdl2.getError())
+    quit(1)
+
+  var
+    win: WindowPtr = sdl2.createWindow("Hello World!", 100, 100, 640, 480, sdl2.SDL_WINDOW_SHOWN)
+
+  if win == nil:
+    echo("SDL2 Error: " & $sdl2.getError())
+    quit(1)
+
+  delay(2000)
+  quit(0)
